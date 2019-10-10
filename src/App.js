a939109b59bdc078219864e134b9f06e0a40c284
Vinicius Dufloth <vinicius.dufloth@gmail.com>
2019-10-10 00:52:23 -0300

    Ex 3

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 
 function App() {
   const [movies, setMovies] = useState([])
+  const [favoritos, setFavoritos] = useState([])
   const [pesquisa, setPesquisa] = useState('');
 
   const handleClick = () => {
@@ -12,9 +13,29 @@ function App() {
       .then(json => setMovies(json.Search))
   }
 
+  const favoriteClick = (id) => {
+    console.log(id);
+    const newMovies 
+      = movies.map(movie => {
+        return movie.imdbID === id ? {...movie, favorite: !movie.favorite} : movie;
+      });
+    const favoritados = newMovies.filter(movie => movie.favorite);
+    setFavoritos(favoritados);
+    setMovies(newMovies);
+  }
+
   return (
-    <>
     <center>
+        <h1>FAVORITADOS</h1>
+        {favoritos && favoritos.map(favorito => {
+            return(
+              <div key={favorito.imdbID}>
+                <div>{favorito.Title} - {favorito.Year}</div>
+                <img border="0" width="100" src={favorito.Poster}></img>
+              </div>
+            );
+          })}
+        <br/>
         <h1>FILMES</h1>
         <input onChange={event => setPesquisa(event.target.value)}/>
         <button onClick={() => handleClick()}>Pesquisar</button>
@@ -24,16 +45,17 @@ function App() {
               <div key={movie.imdbID}>
                 <h2>{movie.Title} - {movie.Year}</h2>
                 <img border="0" src={movie.Poster}></img>
-                {/*<br/>
-                {movie.favorite && <span>Favorito</span>}
-                <button onClick={() => handleClick(movie.imdbID)}>Favoritar</button>*/}
+                <button onClick={() => favoriteClick(movie.imdbID)}>
+                  {!movie.favorite && <div>Favoritar</div>}
+                  {movie.favorite && <div>Desfavoritar</div>}                
+                  </button>
               </div>
             );
           })}
         </span>
      </center>
-    </>
   );
 }
 
 export default App;
+
